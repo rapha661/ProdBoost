1c539fd8f471b432ed25d0e77fdcf11a755ffabb
Raphael Garavati Erbert <83676297+rapha661@users.noreply.github.com>
2024-11-17 04:13:07 +0000

    ajuste para navegação de telas (login e funções)

--- App.js
+++ App.js
@@ -2,7 +2,12 @@ import React, { useState, useEffect } from 'react';
 import {View,Text,TextInput,Button,FlatList,TouchableOpacity,Alert,Vibration,StyleSheet,KeyboardAvoidingView,TouchableWithoutFeedback,Keyboard,Image,Platform} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 // Fechar teclado quando clicar em outro lugar
 function DismissKeyboard({ children }) {
@@ -191,7 +196,7 @@ function PomodoroScreen() {
 
   const resetTimer = () => {
     setIsRunning(false);
-    setIsFocusMode(true); 
+    setIsFocusMode(true); // Retorna sempre para o modo foco ao resetar
     setTimer(focusTime * 60);
   };
 
@@ -275,12 +280,37 @@ function AuthTabs() {
   );
 }
 
+// Stack das telas
+function AppStack() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'To-Do List') {
+            iconName = 'list';
+          } else if (route.name === 'Pomodoro') {
+            iconName = 'clock-o';
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="To-Do List" component={TodoScreen} />
+      <Tab.Screen name="Pomodoro" component={PomodoroScreen} />
+    </Tab.Navigator>
+  );
+}
+
 // Main App
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Auth" component={AuthTabs} />
+        <Stack.Screen name="AppStack" component={AppStack} />
       </Stack.Navigator>
     </NavigationContainer>
   );
